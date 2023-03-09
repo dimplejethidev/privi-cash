273988e97a1f9f919f8a84616e50a060e1f12fbd
theNvN <thenvnsahu@gmail.com>
2023-03-10 00:46:00 +0530

    ui: add network switch modal ui

--- apps/web/components/common/ManagedModal.tsx
+++ apps/web/components/common/ManagedModal.tsx
@@ -1,6 +1,6 @@
 import { Modal, ModalOverlay, ModalContent, ModalBody } from '@chakra-ui/react';
 import { AccountLogIn, AccountRegister } from 'components/account';
-
+import { NetworkSwitch } from 'components/wallet';
 import { modalViews, useUI } from 'contexts/ui';
 
 const ManagedModal: React.FC = () => {
@@ -13,6 +13,7 @@ const ManagedModal: React.FC = () => {
         <ModalBody p={0}>
           {modalView === modalViews.ACCOUNT_REGISTER && <AccountRegister />}
           {modalView === modalViews.ACCOUNT_LOGIN && <AccountLogIn />}
+          {modalView === modalViews.NETWORK_SWITCH && <NetworkSwitch />}
         </ModalBody>
       </ModalContent>
     </Modal>
